e2193370a3f7b8da685bbec8fb77aeaa472ede93
k-kbk <kkbk0077@gmail.com>
2024-02-02 18:37:58 +0900

    feat: 컴포넌트 분리 및 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,15 @@
-'use client';
-import Image from 'next/image';
-import Typewriter from 'typewriter-effect';
+import Introduction from '@/components/home/Introduction';
+import Question from '@/components/home/Question';
+import Recruit from '@/components/home/Recruit';
+import Welcome from '@/components/home/Welcome';
 
 export default function Home() {
   return (
-    <main className="pt-16 font-semibold md:pt-20">
-      <div className="text-right text-sm text-neutral-500 md:text-2xl">
-        명지대학교 IT 서비스 개발 동아리 COW
-      </div>
-      <div className="mt-16 text-xl font-bold md:mt-24 md:text-5xl">
-        세상에 커밋하라:
-      </div>
-      <div className="mt-2 flex text-4xl font-extrabold tracking-tighter text-cowblue md:mt-4 md:text-8xl">
-        <Typewriter
-          options={{
-            strings: ['Commit On World!'],
-            autoStart: true,
-            loop: true,
-          }}
-        />
-      </div>
-      <div className="mt-8 md:mt-14">
-        <button className="border border-neutral-800 bg-cowblue px-4 py-2 text-base transition-opacity hover:opacity-50 md:px-8 md:py-4 md:text-2xl">
-          <a href="https://www.naver.com" target="_blank">
-            3기 지원하기
-          </a>
-        </button>
-      </div>
-      <div className="mt-36 flex w-full justify-center opacity-50">
-        <Image
-          src={'/icon/down-arrow.png'}
-          alt="down-arrow"
-          width={48}
-          height={48}
-        />
-      </div>
+    <main>
+      <Welcome />
+      <Introduction />
+      <Recruit />
+      <Question />
     </main>
   );
 }
---
+++ src/components/home/Welcome.tsx
@@ -0,0 +1,33 @@
+'use client';
+import Typewriter from 'typewriter-effect';
+
+export default function Welcome() {
+  return (
+    <section className="h-[80vh] pt-24 font-semibold">
+      <div className="text-right text-sm text-neutral-500 md:text-2xl">
+        명지대학교 IT 서비스 개발 동아리 COW
+      </div>
+      <div className="mt-16 text-xl font-bold md:mt-24 md:text-5xl">
+        세상에 커밋하라:
+      </div>
+      <div className="mt-2 flex text-4xl font-extrabold tracking-tighter text-cowblue md:mt-4 md:text-8xl">
+        <Typewriter
+          options={{
+            strings: ['Commit On World!'],
+            autoStart: true,
+            loop: true,
+            delay: 75,
+            cursorClassName: 'Typewriter__cursor font-normal text-white',
+          }}
+        />
+      </div>
+      <div className="mt-8 md:mt-14">
+        <button className="border border-neutral-800 bg-cowblue px-4 py-2 text-base transition-opacity hover:opacity-50 md:px-8 md:py-4 md:text-2xl">
+          <a href="https://www.naver.com" target="_blank">
+            3기 지원하기
+          </a>
+        </button>
+      </div>
+    </section>
+  );
+}
