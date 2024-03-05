62742708005133756a1ee224cbde9bd0738b45e4
k-kbk <kkbk0077@gmail.com>
2024-03-05 11:45:20 +0900

    chore: 헤더 로고 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,12 @@
+import Image from 'next/image';
+
 export default function Header() {
   return (
     <header className="fixed top-0 z-50 flex h-14 w-full items-center justify-center bg-black bg-opacity-50 backdrop-blur md:h-16">
       <div className="w-full px-8 text-xl font-bold md:max-w-screen-xl md:px-14 md:text-2xl">
-        카우.
+        <div className="relative h-10 w-10 md:h-11 md:w-11">
+          <Image src={'/icon/logo.png'} fill={true} alt="cow-logo" />
+        </div>
       </div>
     </header>
   );
